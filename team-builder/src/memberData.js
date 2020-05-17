97475689b1a0cccebee21af9ed01ea0439d5f499
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-05-17 00:35:40 -0400

    Update memberData.js

--- team-builder/src/memberData.js
+++ team-builder/src/memberData.js
@@ -6,13 +6,7 @@ const memberData = [
       name: 'Brad Drummonds',
       email: 'brad.drummonds@email.com',
       role: 'Team Lead'
-   },
-   {
-      id: 2,
-      name: 'Jane Doe',
-      email: 'jane.doe@email.com',
-      role: 'UX Design'
-   }
+   } 
 ];
 
 export default memberData;
