b59144b67d350ff709d4de99d4b39174828f00bb
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-05-17 00:41:59 -0400

    Update memberData.js

--- team-builder/src/memberData.js
+++ team-builder/src/memberData.js
@@ -1,12 +1,5 @@
 // Initial data set for list of team members
 
-const memberData = [
-   {
-      id: 1,
-      name: 'Brad Drummonds',
-      email: 'brad.drummonds@email.com',
-      role: 'Team Lead'
-   } 
-];
+const memberData = [];
 
 export default memberData;
